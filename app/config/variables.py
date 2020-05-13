caafc08ed2a16ab11fa06eeba11ee97a8073df38
Mert ÖZ <osemrt@gmail.com>
2020-05-13 22:29:07 +0300

    Add config variables

---
+++ app/config/variables.py
@@ -0,0 +1,9 @@
+from config.ssh_config import HOSTNAME
+from config.ssh_config import USERNAME
+from config.ssh_config import PASSWORD
+from config.ssh_config import PORT
+
+HADOOP_HOME_PATH = '/usr/local/hadoop/hadoop-3.2.1'
+
+
+
